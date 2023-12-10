ba385423517ba111f70796a17f36f42d59f88f21
Salando Linux <saulorlandozzz@gmail.com>
2023-12-10 06:41:46 -0600

    feat: se agrego el modo oscuro

--- src/js/switcher.js
+++ src/js/switcher.js
@@ -4,11 +4,20 @@ const html = document.querySelector("#html")
 const switcher = document.querySelector("#switcher")
 const thumb = document.querySelector("#thumb")
 
-switcher.addEventListener("click", () => {
+const move = () => {
   thumb.classList.toggle("active")
   if (thumb.classList.contains("active")){
     thumb.style.transform = "translateX(calc(-100% - 0.5625rem)"
   }else {
     thumb.style.transform = "translateX(0)"
   }
+}
+
+const change_mode = () => {
+  html.classList.toggle("dark")
+}
+
+switcher.addEventListener("click", () => {
+  move()
+  change_mode()
 })
